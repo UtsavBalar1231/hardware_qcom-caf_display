7a160eef67c51ceb2c33b99e692817db85df4d2b
Thierry Strudel <tstrudel@google.com>
2018-07-26 08:08:18 -0700

    Integration in googleplex
    Change-Id: Ia84e60c5700ed778c9eaaa24ce891f882da4d71c
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter ms% msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
